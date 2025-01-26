3962702b9ead43415ab819a8fb60619407108993
Catchawink <threeeasywords@gmail.com>
2025-01-26 08:08:36 -0500

    Update target.rs

--- src/android/target.rs
+++ src/android/target.rs
@@ -238,7 +238,7 @@ impl<'a> Target<'a> {
         let mut command = CargoCommand::new(mode.as_str())
             .with_verbose(noise_level.pedantic())
             .with_package(Some(config.app().name()))
-            .with_manifest_path(Some(config.app().manifest_path()))
+            //.with_manifest_path(Some(config.app().manifest_path()))
             .with_target(Some(self.triple))
             .with_no_default_features(metadata.no_default_features())
             .with_args(metadata.cargo_args())
@@ -246,7 +246,7 @@ impl<'a> Target<'a> {
             .with_release(profile.release())
             .color(Some(color));
 
-        let args = ["--crate-type=staticlib,cdylib,lib".to_string()];   
+        let args = ["--crate-type=staticlib,cdylib,rlib".to_string()];   
         if mode == CargoMode::Build {
              command = command.with_rustc_args(Some(&args));
         }
