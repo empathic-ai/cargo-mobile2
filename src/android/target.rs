cb4856c6aaa47748ae5909e7d8a6a1d5e89f6f2f
Catchawink <threeeasywords@gmail.com>
2025-01-26 23:36:45 -0500

    Revert "Update target.rs"
    This reverts commit 94aaa9de76247217de7460381eaf0879e3fbb925.

--- src/android/target.rs
+++ src/android/target.rs
@@ -11,7 +11,7 @@ use crate::{
     util::{
         cli::{Report, Reportable},
         CargoCommand,
-    }
+    },
 };
 use once_cell_regex::exports::once_cell::sync::OnceCell;
 use serde::Serialize;
@@ -251,12 +251,6 @@ impl<'a> Target<'a> {
              command = command.with_rustc_args(Some(&args));
         }
 
-        let dir = std::env::current_dir().unwrap();
-
-        duct::cmd("cd", vec!["./../../"])
-            .run()
-            .map_err(|cause| CompileLibError::CargoFailed { mode, cause })?;
-
         command.build(env)
             .env("ANDROID_NATIVE_API_LEVEL", min_sdk_version.to_string())
             .env(
@@ -279,11 +273,6 @@ impl<'a> Target<'a> {
             )
             .run()
             .map_err(|cause| CompileLibError::CargoFailed { mode, cause })?;
-
-        duct::cmd("cd", vec![dir.to_str().unwrap()])
-            .run()
-            .map_err(|cause| CompileLibError::CargoFailed { mode, cause })?;
-
         Ok(())
     }
 
